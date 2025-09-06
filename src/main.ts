5f795b85890f769c244475f4018883c15808fb99
Destiny Ifehor <destechofficial@gmail.com>
2025-09-06 14:14:46 +0100

    updated

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { RequestMethod, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
 import cookieParser from 'cookie-parser';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 async function bootstrap() {
-  //const app = await NestFactory.create(AppModule);
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create(AppModule);
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
   //app.setGlobalPrefix('api');
@@ -17,10 +15,8 @@ async function bootstrap() {
     origin: [process.env.APP_URL as string],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    exposedHeaders: ['set-cookie'],
   });
 
-  app.set('trust proxy', true);
   app.use(cookieParser());
 
   await app.listen(process.env.PORT ?? 3000);
